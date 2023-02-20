22ec95f3c56e1844d6651b2809ada7c94427c020
Johann Beleites <johann.beleites@sonarsource.com>
2023-02-20 10:40:31 +0100

    Move to multiplatform build and compose app in preparation for in-built authentication and GUI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,55 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    java
-    application
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
+    //java
+    //application
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+
+    id("org.jetbrains.compose")
+    id("org.openjfx.javafxplugin")
 }
 
 group = "com.sonarsource.dev"
 version = "0.2"
 
 repositories {
+    google()
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     jcenter()
 }
 
-application {
+/*application {
     mainClass.set("org.sonarsource.dev.quirrus.MainKt")
+}*/
+
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+    }
+
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+
+                implementation(kotlin("stdlib-jdk8"))
+                implementation("com.github.kittinunf.fuel:fuel:2.3.1")
+                implementation("com.github.kittinunf.fuel:fuel-kotlinx-serialization:2.3.1")
+                implementation("me.lazmaid.kraph:kraph:0.6.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+                implementation("com.github.ajalt.clikt:clikt:3.4.0")
+            }
+        }
+        val jvmTest by getting
+    }
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
-}
-
-dependencies {
+/*dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.github.kittinunf.fuel:fuel:2.3.1")
     implementation("com.github.kittinunf.fuel:fuel-kotlinx-serialization:2.3.1")
@@ -31,4 +57,20 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
     implementation("com.github.ajalt.clikt:clikt:3.4.0")
+}*/
+
+compose.desktop {
+    application {
+        mainClass = "org.sonarsource.dev.quirrus.MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "quirrus"
+            packageVersion = "1.0.0"
+        }
+    }
+}
+
+javafx {
+    version = "16"
+    modules = listOf("javafx.controls", "javafx.swing", "javafx.web", "javafx.graphics")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
 rootProject.name = "quirrus"
 
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        kotlin("multiplatform").version(extra["kotlin.version"] as String)
+        kotlin("plugin.serialization").version(extra["kotlin.version"] as String)
+        id("org.jetbrains.compose").version(extra["compose.version"] as String)
+        id("org.openjfx.javafxplugin").version(extra["javafxplugin.version"] as String)
+    }
+}
