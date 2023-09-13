cb180f8d63b4e5e325f747c9712dbc4f03f7212d
Johann Beleites <sdefend_9jftz8pq@esnail.de>
2023-09-13 13:06:07 +0200

    Improve repo text field naming

--- wallboard/src/main/kotlin/com/sonarsource/dev/quirrus/wallboard/Constants.kt
+++ wallboard/src/main/kotlin/com/sonarsource/dev/quirrus/wallboard/Constants.kt
@@ -1,4 +1,4 @@
 package com.sonarsource.dev.quirrus.wallboard
 
-const val CIRRUS_REPO_TEXT_FIELD_LABEL = "Cirrus Repository"
+const val CIRRUS_REPO_TEXT_FIELD_LABEL = "Cirrus Repository (ID or name)"
 const val BRANCHES_FIELD_LABEL = "Branches (comma-separated)"
