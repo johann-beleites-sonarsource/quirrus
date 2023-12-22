13709dd35ea389dba39a80b87fbffe2c446da4b7
Angelo Buono <angelo.buono@sonarsource.com>
2023-12-22 12:33:22 +0100

    Add the Legend for task statuses colors

--- wallboard/src/main/kotlin/com/sonarsource/dev/quirrus/wallboard/Constants.kt
+++ wallboard/src/main/kotlin/com/sonarsource/dev/quirrus/wallboard/Constants.kt
@@ -1,4 +1,5 @@
 package com.sonarsource.dev.quirrus.wallboard
 
+const val TASK_STATUS_LEGEND = "Task Statuses:"
 const val CIRRUS_REPO_TEXT_FIELD_LABEL = "Cirrus Repository (ID or name)"
 const val BRANCHES_FIELD_LABEL = "Branches (comma-separated)"
